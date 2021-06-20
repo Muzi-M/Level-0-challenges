85b5f963e99ecc725a9a4f0350becb0db8876f32
Muzi-M <diff.mlot@gmail.com>
2021-06-20 21:08:04 +0200

    Task 0.2 solution

---
+++ Task-0,2.js
@@ -0,0 +1,10 @@
+var x = 1 + 1 * 2
+var y = (1 + 1) * 2
+var z = 1 + (1 *2)
+var a = 1 + 1 * 2 / 2
+var b = (1 + 1 * 2) / 2
+
+answers = [x, y, z, a, b]
+for (i=0; i < answers.length; i++){
+    console.log(answers[i])
+}
