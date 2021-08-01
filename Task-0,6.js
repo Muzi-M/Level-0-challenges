624606ae64017cae4e0e473692d7f2c751cc7a5a
Muzi-M <diff.mlot@gmail.com>
2021-08-01 20:37:34 +0200

    upgraded function to take in multiple arguments

--- Task-0,6.js
+++ Task-0,6.js
@@ -8,4 +8,14 @@ function maxNumber(firstInt, secondInt, thirdInt){
     }
     
 }
-maxNumber(4, 7, 6);
+
+function maxNumber(){
+    let largestInteger = -Infinity
+    for (let i = 0; i < arguments.length; i++){
+        if (arguments[i] > largestInteger){
+            largestInteger = arguments[i]
+        }
+    }
+    return largestInteger
+}
+maxNumber(4, 7, 6, 9, 4, 1);
