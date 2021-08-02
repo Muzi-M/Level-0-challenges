de475e85d5157eb216f855817d2a62bba7a37b82
Muzi-M <diff.mlot@gmail.com>
2021-08-02 14:59:58 +0200

    upgraded function to take in multiple arguments

--- Task-0,6.js
+++ Task-0,6.js
@@ -1,14 +1,3 @@
-function maxNumber(firstInt, secondInt, thirdInt){
-    if (firstInt - secondInt > 0 && firstInt - thirdInt > 0){
-        return firstInt;
-    }else if (secondInt - firstInt > 0 && secondInt - thirdInt > 0){
-        return secondInt;
-    } else {
-        return thirdInt;
-    }
-    
-}
-
 function maxNumber(){
     let largestInteger = -Infinity
     for (let i = 0; i < arguments.length; i++){
